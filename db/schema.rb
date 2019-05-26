ebf5d247d055553e7e4ca434a8578ae2e7769c29
Joe Jamsky <cba1067950@yahoo.com>
2019-05-26 18:13:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,12 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_store", force: :cascade do |t|
+  create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.string "costume_inventory"
-    t.integer "employee_count"
-    t.boolean "closed"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
     t.string "opening_time"
     t.string "closing_time"
   end
