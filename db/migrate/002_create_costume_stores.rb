38b4951f47b6fce0006f232d1243cc9bcb242c57
Joe Jamsky <cba1067950@yahoo.com>
2019-05-26 18:15:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :costume_inventory 
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamp :opening_time
+      t.timestamps :opening_time
       t.string :closing_time 
     end 
   end
